85304dec69855359053d6f413c2bcbcfcfb4d469
zj <9600348@qq.com>
2026-03-02 08:42:48 +0800

    fix: 使用 @ts-ignore 绕过 TypeScript 推断问题

--- web/src/app/providers.tsx
+++ web/src/app/providers.tsx
@@ -4,5 +4,6 @@ import { LanguageProvider } from "@/hooks/use-language";
 import { ReactNode } from "react";
 
 export function Providers({ children }: { children: ReactNode }): React.JSX.Element {
-  return <LanguageProvider>{children}</LanguageProvider> as React.JSX.Element;
+  // @ts-ignore - TypeScript inference issue with LanguageProvider
+  return <LanguageProvider>{children}</LanguageProvider>;
 }
